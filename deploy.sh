272929a3100e4c3ba2129ade6f7344d067db6809
LohithKuruva <97395173+LohithKuruva@users.noreply.github.com>
2022-01-13 08:52:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@ I am updating the branch in master branch and trying to integrate this into deve
 This is an sh file
 This is used to deploy the resource
 updated a line here
+Adding this line to help demonstrate fetch command
