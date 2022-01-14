7520cb480c64e07c4a2f671f39ff967fbcb19039
Mithun Software Solutions <devopstrainingblr@gmail.com>
2022-01-14 08:07:17 +0530

    removed deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-I am updating the branch in master branch and trying to integrate this into development branch
-This is an sh file
-This is used to deploy the resource
-updated a line here
-Adding this line to help demonstrate fetch command
